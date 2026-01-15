baba2c41623190c2955ebe243da8f2d2eb9b9119
Bam Cortez <bamcortez@Bams-MacBook-Pro.local>
2026-01-15 16:45:06 +0800

    Updated 01/15/2026 | Icon

--- partials/Head.php
+++ partials/Head.php
@@ -5,6 +5,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>BudgetTracker | Bloom</title>
+   <link rel="icon" type="image/png" href="assets/img/icon.png">
    <script src="https://cdn.tailwindcss.com"></script>
    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
